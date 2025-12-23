b34e15c5c1b4ceb2e7ed0f9439aae2f61269986f
Sabrina-d55 <sdhaliwal@i-sites.com>
2025-12-22 16:46:24 -0800

    Update2 jobs-proxy.js

--- netlify/functions/jobs-proxy.js
+++ netlify/functions/jobs-proxy.js
@@ -1,7 +1,6 @@
 // netlify/functions/jobs-proxy.js
 
-// CommonJS style so Netlify is happy regardless of runtime
-exports.handler = async (event) => {
+exports.handler = async () => {
   const WORKDAY_URL =
     'https://services1.myworkday.com/ccx/service/customreport2/benchmark/ISU_Benchmark_Job_Req/Benchmark_Job_Requisitions_XML_-_Deloitte?format=simplexml';
 
@@ -13,20 +12,18 @@ exports.handler = async (event) => {
       console.error('Missing WORKDAY_USERNAME or WORKDAY_PASSWORD');
       return {
         statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        },
         body: 'Server not configured with Workday credentials.'
       };
     }
 
-    // Build Basic Auth header
     const authHeader =
       'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
 
-    // Multi-origin CORS support
-    const allowedOrigins = (process.env.ALLOWED_ORIGINS || '').split(',');
-    const origin = event.headers.origin;
-    const corsOrigin = allowedOrigins.includes(origin) ? origin : '';
-
-    // Call Workday
     const res = await fetch(WORKDAY_URL, {
       method: 'GET',
       headers: {
@@ -42,7 +39,7 @@ exports.handler = async (event) => {
       return {
         statusCode: res.status,
         headers: {
-          'Access-Control-Allow-Origin': corsOrigin,
+          'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET,OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         },
@@ -50,12 +47,11 @@ exports.handler = async (event) => {
       };
     }
 
-    // Success: return XML to browser
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/xml',
-        'Access-Control-Allow-Origin': corsOrigin,
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization'
       },
@@ -65,6 +61,11 @@ exports.handler = async (event) => {
     console.error('Proxy error:', err);
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      },
       body: `Proxy error: ${err.message || 'Unknown error'}`
     };
   }
